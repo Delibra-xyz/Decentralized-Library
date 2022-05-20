606a8cb4a0998aaa4fdcbc365d9d6e2a0c946660
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2022-05-20 09:15:31 +0100

    edited  book struct

--- hardhat-tutorial/contracts/library.sol
+++ hardhat-tutorial/contracts/library.sol
@@ -11,19 +11,33 @@ contract OpenBooks{
 struct FreeBooks {
     string ipfsCID;
     string bookName;
+    string bookAuthor;  // author of book
+    string description; // about the book
+    uint year;   // year of publication
     string genre;
     uint timeUploaded;
     string author;
     string free;
+    uint buyPrice;
+    uint borrowPrice;
+    string edition;
+    uint ratings;
 }
 
 struct PaidBooks {
     string ipfsCID;
     string bookName;
+    string bookAuthor;  // author of book
+    string description; // about the book
+    uint year;   // year of publication
     string genre;
     uint timeUploaded;
     string author;
     string paid;
+    uint buyPrice;
+    uint borrowPrice;
+    string edition;
+    uint ratings;
 }
 mapping (string => BookDetail) public aboutBook;
 mapping (address => uint) name;
@@ -44,10 +58,10 @@ constructor() {
 /// @param  _ipfsCID The CID hash of the file uploaded to ipfs/any decentralised storage
 /// @param  _fileName The file name of the file
 
-function fileUpload(string memory _ipfsCID, string memory _bookName, string bookType) public {
+function fileUpload(string memory _ipfsCID, string memory _bookName,string memory _bookAuthor, string memory _description, uint memory _year, uint memory _genre, string _bookType, uint memory _buy, uint memory _borrow, string memory _edition ) public {
     require(bookExists[_bookName] == false, "Book with this Name already exists, Rename");
 
-    BookDetail memory bookDetails = BookDetail(_ipfsCID, _fileName, _genre, block.timestamp, msg.sender, _bookType);
+    BookDetail memory bookDetails = BookDetail(_ipfsCID, _fileName,_fileAuthor, _desc, _year, _genre, block.timestamp, msg.sender, _bookType, _buy, _borrow, _edition, 0);
 
     bookExists[_bookName] = true;
     bookExists[_ipfsCID] = true;
