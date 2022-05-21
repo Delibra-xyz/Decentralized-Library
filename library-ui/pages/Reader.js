1b965045ba525fdb8535929719801d057c59f97e
Khadeeejah <wuraola81@gmail.com>
2022-05-21 07:06:00 +0200

    readers-

--- library-ui/pages/Reader.js
+++ library-ui/pages/Reader.js
@@ -1,62 +1,23 @@
-import Link from "next/link";
-import React from "react";
-import { Box, Flex, Heading, Avatar } from "@chakra-ui/react";
-import BookCard from "../components/BookCard";
+import React, { useEffect } from "react";
+import Layout from "../components/Layout";
+import AboutBook from "../components/AboutBook";
+import { useRouter } from "next/router";
+import { Reader } from "@ethersproject/abi/lib/coders/abstract-coder";
+import Author from "./Author";
 
-
-
-const Reader = (props)=> {
+const Readerbook = (props) => {
+  const router = useRouter();
+  // useEffect(()=> {
+  //         if(!props.account?.address){
+  //             router.push('/');
+  //         }
+  // },[props])
   return (
-    <>
-     <Avatar name="Anonymous" src="dp.png" size="lg" />
-      <Box mt={10} p="30px">
-        <Heading mb={5}>Featured</Heading>
-        {console.log(props)}
-
-        <Flex direction={{ base: "column", md: "row" }}>
-          <BookCard
-            title="Title1"
-            author="Lorem Ipsum"
-            image="featured1.jfif"
-            w="20%"
-            account={props.account}
-          />
-
-          <BookCard
-            title="Title2"
-            author="Lorem Ipsum"
-            image="featured2.jfif"
-            w="20%"
-            account={props.account}
-          />
-
-          <BookCard
-            title="Title3"
-            author="Lorem Ipsum"
-            image="featured3.jfif"
-            w="20%"
-            account={props.account}
-          />
-
-          <BookCard
-            title="Title4"
-            author="Lorem Ipsum"
-            image="featured4.jfif"
-            w="20%"
-            account={props.account}
-          />
-
-          <BookCard
-            title="Title5"
-            author="Lorem Ipsum"
-            image="featured5.jfif"
-            w="20%"
-            account={props.account}
-          />
-        </Flex>
-      </Box>
-    </>
+    <Layout>
+    
+      <AboutBook />
+    </Layout>
   );
 };
 
-export default Reader;
+export default Readerbook;
