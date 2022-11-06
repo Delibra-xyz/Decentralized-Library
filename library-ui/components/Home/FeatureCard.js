67216210db009656f553402db5115f0697241753
Anyanwu Maureen <maureenanyanwu519@gmail.com>
2022-11-06 19:45:18 +0100

    added rainbowkit, features and goals section

---
+++ library-ui/components/Home/FeatureCard.js
@@ -0,0 +1,40 @@
+import { Box, IconButton, Text } from "@chakra-ui/react";
+import React from "react";
+
+const FeatureCard = ({icon, title, cont})=> {
+    return(
+        <Box
+            w="20%"
+            minW="250px"
+            maxW="280px"
+            mr={5}
+        >
+            <IconButton
+                bg="linear-gradient(115.03deg, #FFB0BD 6.95%, #FFC2A1 89.09%)"
+                color="#fff"
+                aria-label='Call Segun'
+                borderRadius="10px"
+                icon={icon}
+                mb={4}
+                className="heartbeat"
+            />
+            <Text
+                bgGradient="linear-gradient(98.41deg, #02081F 0%, #1A0830 96.87%)"
+                bgClip='text'
+                fontSize="22px"
+                fontWeight="700"
+                mb={4}
+            >{title}</Text>
+            <Text
+                color="#1F2937"
+                fontSize="16px"
+                fontWeight="500"
+            >
+                {cont} 
+            </Text>
+
+        </Box>
+    )
+}
+
+export default FeatureCard;
--- library-ui/pages/_app.js
+++ library-ui/pages/_app.js
@@ -1,10 +1,39 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/style.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  // ConnectButton,
+  getDefaultWallets,
+  RainbowKitProvider
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [alchemyProvider({ alchemyId: "1O-RmHgRukhIxw2je_L5iB7aNlVNRho2" }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
 
 function Home({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ChakraProvider>
   )
 }
