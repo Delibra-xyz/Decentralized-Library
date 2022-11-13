ebd86e7514e0697e339bdd9c6afb8bc68733ac3d
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-11-13 14:23:25 +0100

    How it works section

---
+++ library-ui/pages/HowItWorks/authorData.js
@@ -0,0 +1,22 @@
+export const authorData = [
+  {
+    id: 1,
+    step: '1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id dictum augue mi vestibulum tempor.',
+  },
+  {
+    id: 2,
+    step: '2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id dictum augue mi vestibulum tempor.',
+  },
+  {
+    id: 3,
+    step: '3',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id dictum augue mi vestibulum tempor.',
+  },
+  {
+    id: 4,
+    step: '4',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id dictum augue mi vestibulum tempor.',
+  },
+];
