9a5d984da2dd2d8265b8b570b1237a23f32ab1d7
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-11-29 10:18:09 +0100

    updates to dashboard header

--- library-ui/components/Dashboard/DashboardHeader.js
+++ library-ui/components/Dashboard/DashboardHeader.js
@@ -1,4 +1,4 @@
-import { Image } from '@chakra-ui/react';
+import { Button, Image, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import Bell from '../../assets/svgs/Bell';
 import Ethereum from '../../assets/svgs/ethereum';
 import ChevronDown from '../../assets/svgs/ChevronDown';
@@ -13,18 +13,48 @@ const DashboardHeader = () => {
           <div className={styles.dashboardHeader__notification}>
             <Bell />
           </div>
-          <div className={styles.dashboardHeader__dropdown}>
-            <div className={styles.dashboardHeader__dropdown__box}>
-              <Ethereum />
-            </div>
-            <span>Ethereum</span>
-            <ChevronDown />
-          </div>
-          <div className={styles.dashboardHeader__dropdown}>
-            <Image src='metamask.png' alt='metamask' />
-            <span>0x9...10Q</span>
-            <ChevronDown />
-          </div>
+          <Menu>
+            <MenuButton
+              as={Button}
+              leftIcon={
+                <div className={styles.dashboardHeader__dropdown__box}>
+                  <Ethereum />
+                </div>
+              }
+              rightIcon={<ChevronDown />}
+              backgroundColor='#e5e7eb'
+              mr='16px'
+              borderRadius='8px'
+              fontWeight={600}
+              _focus={{
+                boxShadow: 'none',
+              }}
+            >
+              Ethereum
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Hello</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              as={Button}
+              leftIcon={<Image src='metamask.png' alt='metamask' />}
+              rightIcon={<ChevronDown />}
+              backgroundColor='#f3f4f6'
+              mr='16px'
+              borderRadius='8px'
+              fontWeight={600}
+              _focus={{
+                boxShadow: 'unset',
+              }}
+            >
+              0x9...10Q
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Hello</MenuItem>
+            </MenuList>
+          </Menu>
           <Image src='/dp.png' alt='avatar' width='40px' height='40px' borderRadius='50%' />
         </div>
       </div>
