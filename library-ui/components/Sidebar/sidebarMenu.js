b65c4c8b654c8b1c26b85b4ec796bd6cbc875d15
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-12-01 19:02:35 +0100

    add reader routes

--- library-ui/components/Sidebar/sidebarMenu.js
+++ library-ui/components/Sidebar/sidebarMenu.js
@@ -1,31 +1,65 @@
 import Grid from '../../assets/svgs/grid';
+import Fire from '../../assets/svgs/fire';
 import BookOpen from '../../assets/svgs/BookOpen';
 import Gift from '../../assets/svgs/gift';
 import Settings from '../../assets/svgs/settings';
 
 export const authorRoutes = [
   {
-    to: '/dashboard',
+    to: '/author/overview',
     routeIcon: <Grid />,
     routeName: 'Dashboard',
-    tag:"overview"
+    tag: 'overview',
   },
   {
-    to: '/dashboard/library',
+    to: '/author/library',
     routeIcon: <BookOpen />,
     routeName: 'My library',
-    tag:"library"
+    tag: 'library',
   },
   {
-    to: '/dashboard',
+    to: '/author',
     routeIcon: <Gift />,
     routeName: 'Rewards',
-    tag:"rewards"
+    tag: 'rewards',
   },
   {
-    to: '/dashboard',
+    to: '/author',
     routeIcon: <Settings />,
     routeName: 'Settings',
-    tag:"settings"
+    tag: 'settings',
+  },
+];
+
+export const readerRoutes = [
+  {
+    to: '/reader/home',
+    routeIcon: <Grid />,
+    routeName: 'Home',
+    tag: 'home',
+  },
+  {
+    to: '/reader/browse',
+    routeIcon: <Fire />,
+    routeName: 'Browse',
+    tag: 'browse',
+  },
+  {
+    to: '/reader/library',
+    routeIcon: <BookOpen />,
+    routeName: 'My library',
+    tag: 'library',
+  },
+  {
+    to: '/reader',
+    routeIcon: <Gift />,
+    routeName: 'Rewards',
+    tag: 'rewards',
+  },
+  {
+    to: '/reader',
+    routeIcon: <Settings />,
+    routeName: 'Settings',
+    tag: 'settings',
   },
 ];
