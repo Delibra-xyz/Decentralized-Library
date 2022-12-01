7e3d31ceb6fe30db135ed6dab867d5dc01e31c2f
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-12-01 19:03:48 +0100

    book overview page

---
+++ library-ui/assets/svgs/share.js
@@ -0,0 +1,107 @@
+const Share = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      width='24'
+      height='25'
+      viewBox='0 0 24 25'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M18 2.96796C17.2044 2.96796 16.4413 3.28403 15.8787 3.84664C15.3161 4.40925 15 5.17231 15 5.96796C15 6.76361 15.3161 7.52667 15.8787 8.08928C16.4413 8.65189 17.2044 8.96796 18 8.96796C18.7956 8.96796 19.5587 8.65189 20.1213 8.08928C20.6839 7.52667 21 6.76361 21 5.96796C21 5.17231 20.6839 4.40925 20.1213 3.84664C19.5587 3.28403 18.7956 2.96796 18 2.96796Z'
+        stroke='url(#paint0_linear_1017_5732)'
+        strokeWidth='1.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M6 9.96796C5.20435 9.96796 4.44129 10.284 3.87868 10.8466C3.31607 11.4092 3 12.1723 3 12.968C3 13.7636 3.31607 14.5267 3.87868 15.0893C4.44129 15.6519 5.20435 15.968 6 15.968C6.79565 15.968 7.55871 15.6519 8.12132 15.0893C8.68393 14.5267 9 13.7636 9 12.968C9 12.1723 8.68393 11.4092 8.12132 10.8466C7.55871 10.284 6.79565 9.96796 6 9.96796Z'
+        stroke='url(#paint1_linear_1017_5732)'
+        strokeWidth='1.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M18 16.968C17.2044 16.968 16.4413 17.284 15.8787 17.8466C15.3161 18.4092 15 19.1723 15 19.968C15 20.7636 15.3161 21.5267 15.8787 22.0893C16.4413 22.6519 17.2044 22.968 18 22.968C18.7956 22.968 19.5587 22.6519 20.1213 22.0893C20.6839 21.5267 21 20.7636 21 19.968C21 19.1723 20.6839 18.4092 20.1213 17.8466C19.5587 17.284 18.7956 16.968 18 16.968V16.968Z'
+        stroke='url(#paint2_linear_1017_5732)'
+        strokeWidth='1.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M8.58984 14.478L15.4198 18.458'
+        stroke='url(#paint3_linear_1017_5732)'
+        strokeWidth='1.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M15.4098 7.47796L8.58984 11.458'
+        stroke='url(#paint4_linear_1017_5732)'
+        strokeWidth='1.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <defs>
+        <linearGradient
+          id='paint0_linear_1017_5732'
+          x1='15'
+          y1='2.96796'
+          x2='21.7401'
+          y2='3.96501'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#02081F' />
+          <stop offset='0.96875' stopColor='#1A0830' />
+        </linearGradient>
+        <linearGradient
+          id='paint1_linear_1017_5732'
+          x1='3'
+          y1='9.96796'
+          x2='9.74008'
+          y2='10.965'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#02081F' />
+          <stop offset='0.96875' stopColor='#1A0830' />
+        </linearGradient>
+        <linearGradient
+          id='paint2_linear_1017_5732'
+          x1='15'
+          y1='16.968'
+          x2='21.7401'
+          y2='17.965'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#02081F' />
+          <stop offset='0.96875' stopColor='#1A0830' />
+        </linearGradient>
+        <linearGradient
+          id='paint3_linear_1017_5732'
+          x1='8.58984'
+          y1='14.478'
+          x2='15.9555'
+          y2='16.3478'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#02081F' />
+          <stop offset='0.96875' stopColor='#1A0830' />
+        </linearGradient>
+        <linearGradient
+          id='paint4_linear_1017_5732'
+          x1='8.58984'
+          y1='7.47796'
+          x2='15.946'
+          y2='9.34266'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#02081F' />
+          <stop offset='0.96875' stopColor='#1A0830' />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default Share;
