70c6a0cec8554229f056c7b9aabc03357c5252a3
Khadeeejah <wuraola81@gmail.com>
2023-01-02 01:00:15 +0400

    roadmap-off

--- library-ui/components/roadmap.jsx
+++ library-ui/components/roadmap.jsx
@@ -1,46 +1,24 @@
-import { Heading, Text, Image, SimpleGrid } from '@chakra-ui/react'
-import RoadmapStepper from './Home/RoadmapStepper'
+import { Heading, Image, SimpleGrid } from '@chakra-ui/react'
 
 const Roadmap = () => {
   return (
     <div className="roadmap">
-      <Heading
-        fontFamily="'Clash Grotesk', sans-serif"
-        fontSize="56px"
-        color="#FFFFFF"
-        fontWeight="700"
-        mb={5}
-        textAlign="center"
-      >
-        Delibra Roadmap
-      </Heading>
-      <Text
-        color="#E5E7EB"
-        fontSize="20px"
-        fontWeight="500"
-        mb={5}
-        textAlign="center"
-        w={{base:"100%", sm:"85%", md:"70%"}}
-        mx="auto"
-      >
-        We have fascinating milestones to achieve, here is our project
-        development phase.
-      </Text>
-      <RoadmapStepper />
-
       <div className="Footer-logo">
         <Heading
           fontFamily="'Clash Grotesk', sans-serif"
           fontSize="56px"
           color="#FFFFFF"
           fontWeight="700"
-          mt={40}
+          mt={5}
           mb={20}
           textAlign="center"
         >
           Powered By
         </Heading>
-        <SimpleGrid columns={{base:3, md:5}} spacing={{base:"20px", md:"40px"}}>
+        <SimpleGrid
+          columns={{ base: 3, md: 5 }}
+          spacing={{ base: '20px', md: '40px' }}
+        >
           <Image
             src="https://res.cloudinary.com/polygontech/image/upload/f_auto,q_auto,dpr_2,w_44,h_44/Polygon_Hermez_efab9004dc"
             alt="Polygon zkEVM"
@@ -74,28 +52,28 @@ const Roadmap = () => {
 
           <Image
             width={200}
-            ml={{base:0, md:20}}
+            ml={{ base: 0, md: 20 }}
             src="https://raw.githubusercontent.com/chakra-ui/chakra-ui/main/media/logo-colored@2x.png?raw=true"
             alt="Dan Abramov"
           />
 
           <Image
             width={200}
-            ml={{base:0, md:20}}
+            ml={{ base: 0, md: 20 }}
             src="https://en.bitcoinwiki.org/upload/en/images/thumb/d/d5/Solidity.png/400px-Solidity.png"
             alt="Dan Abramov"
           />
 
           <Image
             width={200}
-            ml={{base:0, md:20}}
+            ml={{ base: 0, md: 20 }}
             src="https://miro.medium.com/max/1000/1*htbUdWgFQ3a94PMEvBr_hQ.png"
             alt="Dan Abramov"
           />
 
           <Image
             width={150}
-            ml={{base:0, md:20}}
+            ml={{ base: 0, md: 20 }}
             src="https://miro.medium.com/max/420/1*3jj5tQildSIyhl-RO6RLlA.png"
             alt="Dan Abramov"
           />
